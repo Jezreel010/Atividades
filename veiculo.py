73d1e812bb252cf7debcb4741283c0d042423702
Jezreel010 <164887522+Jezreel010@users.noreply.github.com>
2024-04-19 11:22:36 -0300

    Atividade de herança 19/04/2024

---
+++ veiculo.py
@@ -0,0 +1,37 @@
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        
+def frear(self):
+    print(f"{self.marca} está freando")
+
+def  acelerar(self):
+    print(f"O {self.marca} está acelerando")
+
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, cor):
+        super().__init__(marca, modelo)
+        self.cor = cor
+
+    def abrir_porta(self):
+        print(f"A {self.modelo} está abrindo a porta")
+
+class Moto (Veiculo):
+    def __init__(self, marca, modelo, cilindrada):
+        super().__init__(marca, modelo)
+        self.cilindrada = cilindrada
+
+    def empinar(self):
+        print(f"A{self.modelo} está empinando")
+
+veiculos_geral = Veiculo("motos", "Veiculos")
+carro1 = Carro("lamborghini" ,"Veneno", "preto")
+moto1  = Moto("Honda", "CBR", "1000")
+
+veiculos_geral.frear()
+veiculos_geral.acelerar()
+carro1.abrir_porta()
+moto1.empinar()
+
+
